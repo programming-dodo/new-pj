04fbb9ca408ecfc05b585f13c6842f638ce35850
Jeremiah Farrar <bruceakaspider@gmail.com>
2024-04-15 13:08:36 -0400

    added more data in store and page

--- app.vue
+++ app.vue
@@ -1,33 +1,30 @@
 <script setup lang="ts">
-import { useFetch } from "nuxt/app";
-const api_token = process.env.YNAB_TOKEN
 
 const budgetStore = useBudgetStore()
+var selectedBudget  = ref()
+var selectedCategory = ref()
 
-const {data:budgets} = await useFetch('https://api.ynab.com/v1/budgets', {
-  headers: { Authorization: `Bearer ${api_token}` }
-})
-
-const mb = ref(budgets.value.data.budgets)
-
-function gBud()  {
-  budgetStore.getBudgets()
-}
-const yb = budgetStore.budgets
-
-// console.log(budgets)
-
-// onBeforeMount(() => {
-//   budgetStore.getBudgets()
-// })
 </script>
 
 <template>
-  <div>
-    {{ mb }}
-    <hr>
-    <button @click="gBud">Get Budgets</button>
-    <hr>
-    {{ yb }}
-  </div>
+<div>
+  <button @click="budgetStore.getPayee(selectedBudget.id)" v-if="selectedBudget != null">Payees</button>
+<button @click="budgetStore.getCategories(selectedBudget.id)" v-if="selectedBudget != null">
+  displayCategories
+</button>
+  <button @click="budgetStore.getBudgets()" v-if="selectedBudget == null">Load Data</button>
+  <select v-model="selectedBudget">
+    <option disabled >budget</option>
+    <option v-for="budget in budgetStore.budgets" :value="budget" :key="budget.id">
+      {{ budget.name }}
+    </option>
+  </select>
+  <select v-model="selectedCategory" v-if="budgetStore.categories.length != 0">
+    <option v-for="cat in budgetStore.categories" :key="cat.id" :value="cat">{{ cat.name }}</option>
+  </select>
+  <select v-if="budgetStore.payees.length != 0">
+    <option v-for="payee in budgetStore.filterPayees" :key="payee?.id" :value="payee"> {{ payee?.name }}</option>
+  </select>
+  <p>{{ selectedCategory?.name }}</p>
+</div>
 </template>
--- stores/BudgetStore.ts
+++ stores/BudgetStore.ts
@@ -1,24 +1,54 @@
-import { useFetch } from "nuxt/app";
 import { defineStore } from "pinia";
 
 export const useBudgetStore = defineStore('budgets', {
     state:  () => ({
       token: process.env.YNAB_TOKEN as string,
-      budgets: []
+      budgets: [],
+      categories: [],
+      payees: [],
     }), 
     actions: {
       async getBudgets() {
         await $fetch('https://api.ynab.com/v1/budgets', {
           headers: { Authorization: `Bearer ${this.token}` }
         }).then((response) => {
-          console.log({tb: this.budgets, r: response.data.budgets})
           this.budgets.length = 0
           response.data.budgets.forEach((element) => this.budgets.push(element));
         })
+      },
+      async getCategories(id: string) {
+        await $fetch(`https://api.ynab.com/v1/budgets/${id}/categories`, {
+          headers: { Authorization: `Bearer ${this.token}`}
+        }).then((response) => {
+          this.categories.length = 0
+          response.data.category_groups.forEach((element) => this.categories.push(element))
+        })
+      },
+      async getPayee(budgetId: string) {
+        await $fetch(`https://api.ynab.com/v1/budgets/${budgetId}/payees`, {
+          headers: {Authorization: `Bearer ${this.token}`}
+        }).then((response) => {
+          this.payees.length=0
+          response.data.payees.forEach((element) => this.payees.push(element))
+        })
       }
     },
-    getters: {},
+    getters: {
+      filterPayees() {
+        var name1 = "Starting Balance"
+        var name2 = "Manual Balance Adjustment"
+        var name3 = "Reconciliation Balance Adjustment"
+        var retPayees: [] = []
+        this.payees.forEach((payee) => {
+          if(payee?.transfer_account_id == null && payee.name != name1 && payee.name != name2 && payee.name != name3) {
+            retPayees.push(payee)
+          }
+        })
+        return retPayees
+      }
+    },
 })
+
 if (import.meta.hot) {
   import.meta.hot.accept(acceptHMRUpdate(useBudgetStore, import.meta.hot))
 }
